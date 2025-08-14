d2af43b3ae71199a83c2eab36c19e57bbc459b0e
TheAlvi <maulvibebekterbang@gmail.com>
2025-08-14 20:44:08 +0700

    ESLint rules, not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,12 @@
 import Layout from '@/components/Layout';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <Layout>
         <h1 className="text-8xl font-bold">404</h1>
         <p>Laman tidak ditemukan.</p>
-        <a href="/" className="delay-150 duration-300 underline hover:text-blue-400 hover:underline-offset-2">Kembali ke Beranda</a>
+        <Link href="/" className="delay-150 duration-300 underline hover:text-blue-400 hover:underline-offset-2">Kembali ke Beranda</Link>
     </Layout>
   );
 }
